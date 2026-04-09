23786f6b4886ec8f8c76aa58cfcb5b8f2f54eb5f
Husoschi Ionut-Catalin <24230102+ionut@users.noreply.github.com>
2026-04-09 15:00:57 +0300

    chore: update cosmoz-tokens to v3.4.0 and add a11y addon (#11)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 export default {
 	stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-	addons: ['@storybook/addon-docs'],
+	addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
 	framework: '@storybook/web-components-vite',
 };
