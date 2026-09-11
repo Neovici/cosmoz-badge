61b116185b6a576757d6d9d453b9f233387c3d19
fredrikbernholm <35036205+fredrikbernholm@users.noreply.github.com>
2026-09-11 08:36:06 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-badge@0ec2f04be3a902d418d8af785808c51e0307d372 🚀

--- assets/cosmoz-badge.stories-D6RLQ-XX.js
+++ assets/cosmoz-badge.stories-D6RLQ-XX.js
@@ -1,4 +1,4 @@
-import{A as so,b as l,w as M,D as ro}from"./iframe-CpFNmd4y.js";import"./preload-helper-PPVm8Dsz.js";const z=o=>o??so;function A(o,e,s){return o?e(o):s?.(o)}const a=({slot:o,title:e,className:s,width:r="24",height:c="24",styles:i}={})=>l`
+import{A as so,b as l,w as M,D as ro}from"./iframe-BS3Ty9hI.js";import"./preload-helper-PPVm8Dsz.js";const z=o=>o??so;function A(o,e,s){return o?e(o):s?.(o)}const a=({slot:o,title:e,className:s,width:r="24",height:c="24",styles:i}={})=>l`
   <svg
     slot=${z(o)}
     class=${`arrow-right-icon ${s??""}`}
@@ -311,9 +311,17 @@ import{A as so,b as l,w as M,D as ro}from"./iframe-CpFNmd4y.js";import"./preload
 	 * ========================================= */
 
 	:host([color='brand']) .badge {
-		background-color: var(--cz-color-brand-50);
-		color: var(--cz-color-brand-800);
-		border-color: var(--cz-color-brand-300);
+		background-color: color-mix(
+			in oklab,
+			var(--cz-color-fg-brand) 10%,
+			var(--cz-color-bg-primary)
+		);
+		color: var(--cz-color-text-brand);
+		border-color: color-mix(
+			in oklab,
+			var(--cz-color-fg-brand) 40%,
+			transparent
+		);
 	}
 
 	:host([color='error']) .badge {
