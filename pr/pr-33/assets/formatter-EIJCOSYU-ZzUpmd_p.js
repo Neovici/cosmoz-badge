4fa0bd80ede23fbd2543bb5479b472914531bae9
fredrikbernholm <fredrikbernholm@users.noreply.github.com>
2026-09-24 12:04:43 +0000

    Deploy preview for PR 33 🛫

---
+++ pr/pr-33/assets/formatter-EIJCOSYU-ZzUpmd_p.js
@@ -0,0 +1,1 @@
+import{d as m,b as e}from"./DocsRenderer-LL677BLK-Mq6-KqrX.js";import{K as o}from"./iframe-C5CEYP_W.js";import"./preload-helper-PPVm8Dsz.js";import"./_commonjsHelpers-CqkleIqs.js";var t=o(e(),1),s=(0,t.default)(2)(async(i,r)=>i===!1?r:m(r));export{s as formatter};
